5f83221d8f451a7966f638661fc28c46b22b5fd3
Paul Morrow <pmorow93@gmail.com>
2022-09-30 15:52:14 -0500

    updated tailwind config

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main>
+        <section className="bg-blue-500 h-screen">
+          <h1>Hello there!</h1>
+        </section>
+      </main>
     </div>
   );
 }
